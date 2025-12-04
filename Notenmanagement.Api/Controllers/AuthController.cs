0c6358adaf0fc7636612affd181063881823cd7e
Nikola Matic <Nmatic@online.gibz.ch>
2025-12-04 10:26:06 +0100

    Added Project Structure
    3 Class Librarys with classes
    1 Api with classes

---
+++ Notenmanagement.Api/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notenmanagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        //Niki
+    }
+}
