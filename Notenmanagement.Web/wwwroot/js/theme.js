21052a5cdce222fbe25c499160af972d26b3a133
MelihYasar <melihsvs58@outlook.com>
2026-01-18 19:32:17 +0100

    add dark & light mode

---
+++ Notenmanagement.Web/wwwroot/js/theme.js
@@ -0,0 +1,13 @@
+﻿window.theme = {
+    get: () => localStorage.getItem("theme") || "light",
+    set: (value) => {
+        const v = value === "dark" ? "dark" : "light";
+        localStorage.setItem("theme", v);
+        document.documentElement.setAttribute("data-bs-theme", v);
+    },
+    init: () => {
+        const v = localStorage.getItem("theme") || "light";
+        document.documentElement.setAttribute("data-bs-theme", v);
+        return v;
+    }
+};
